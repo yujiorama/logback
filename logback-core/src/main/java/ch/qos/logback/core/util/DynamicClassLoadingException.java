674246b5d06638e01c61fd31150338c5c2ff61c3
Joern Huxhorn <jhuxhorn@googlemail.com>
2011-11-06 15:03:28 +0100

    Added c'tor without cause-Throwable.

--- logback-core/src/main/java/ch/qos/logback/core/util/DynamicClassLoadingException.java
+++ logback-core/src/main/java/ch/qos/logback/core/util/DynamicClassLoadingException.java
@@ -17,6 +17,10 @@ public class DynamicClassLoadingException extends Exception {
 
   private static final long serialVersionUID = 4962278449162476114L;
 
+  public DynamicClassLoadingException(String message) {
+    super(message);
+  }
+
   public DynamicClassLoadingException(String desc, Throwable root ) {
     super(desc, root);
   }
