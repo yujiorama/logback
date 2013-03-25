e55752559c2400b23e642066ec100449760d36e8
Ceki Gulcu <ceki@qos.ch>
2013-03-25 21:20:21 +0100

    use concurrentHashMap

--- logback-classic/src/test/java/org/slf4j/impl/SimpleLoggerFactory.java
+++ logback-classic/src/test/java/org/slf4j/impl/SimpleLoggerFactory.java
@@ -3,6 +3,8 @@ package org.slf4j.impl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.slf4j.Logger;
 import org.slf4j.ILoggerFactory;
@@ -17,16 +19,29 @@ import org.slf4j.ILoggerFactory;
 public class SimpleLoggerFactory {
   final static SimpleLoggerFactory INSTANCE = new SimpleLoggerFactory();
 
-   Map loggerMap;
+  //ConcurrentMap<String, Logger> loggerMap;
+  Map loggerMap;
+
 
    public SimpleLoggerFactory() {
      loggerMap = new HashMap();
+     //loggerMap = new ConcurrentHashMap<String, Logger>();
+
    }
 
    /**
     * Return an appropriate {@link SimpleLogger} instance by name.
     */
    public Logger getLogger(String name) {
+//     Logger simpleLogger = loggerMap.get(name);
+//       if (simpleLogger != null) {
+//         return simpleLogger;
+//       } else {
+//         Logger newInstance = new SimpleLogger(name);
+//         Logger oldInstance = loggerMap.putIfAbsent(name, newInstance);
+//         return oldInstance == null ? newInstance : oldInstance;
+//       }
+
      Logger slogger = null;
     // protect against concurrent access of the loggerMap
      synchronized (this) {
